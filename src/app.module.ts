7a44565c54363e0bd081725043b82372dbeb610f
AndersonMarrone <anderson.bezerra@santander.com.br>
2022-03-18 17:19:35 -0300

    develop

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'),
+    TasksModule,
+    UsersModule,
+    AuthModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
